19a157d1f76347c45cf1110b0d2f84cea2cb9b63
Toba Sarmad <toba.sarmad@learnet.se>
2025-09-10 16:06:07 +0200

    Created 2 classes Order and User

---
+++ ITSystem/Order.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITSystem
+{
+    internal class Order
+    {
+        public int Id { get; set; }
+
+        [Required, MaxLength(128)] public string CustomerName { get; set; } = string.Empty;
+        [Required, MaxLength(128)] public string Item { get; set; } = string.Empty;
+
+        [Range(1, 10000)] public int Quantity { get; set; }
+
+        public OrderStatus Status { get; set; } = OrderStatus.New;
+        public DateTime CreatedUtc { get; set; } = DateTime.UtcNow;
+    }
+}
---
+++ ITSystem/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITSystem
+{
+    internal class User
+    {
+        public int Id { get; set; }
+        [Required] public string Username { get; set; } = string.Empty;
+        [Required] public string PasswordHash { get; set; } = string.Empty;
+       
+    }
+}
