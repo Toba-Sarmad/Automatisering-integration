a41e4c42c2c1fea71cd5926df3a680d16df74f89
Toba Sarmad <toba.sarmad@learnet.se>
2025-09-11 13:28:58 +0200

    created an IT system console application that functions as a small order system and a login system

--- ITSystem/Program.cs
+++ ITSystem/Program.cs
@@ -1,10 +1,73 @@
-﻿namespace ITSystem
+﻿using ITSystem.Context;
+using ITSystem.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+
+namespace ITSystem
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            SeedAdmin(db);
+
+            Console.WriteLine("Välkommen till IT-Systemet. Logga in");
+            Console.Write("Användarnamn: ");
+            var user = Console.ReadLine(); string.Empty;
+            Console.Write("Lösenord: ");
+            var pass = ReadPassword();
+
+            if(!Authenticate(db, user, pass))
+            {
+                Console.WriteLine("[SÄKERHET] Inloggning misslyckades.");
+                return;
+            }
+
+            Console.WriteLine("Inloggning lyckades.\n");
+
+            while(true)
+            {
+                Console.WriteLine("1) Lista ordrar");
+                Console.WriteLine("2) Skapa ny order");
+                Console.WriteLine("3) Avsluta");
+                Console.Write("Val: "); 
+                var choice = Console.ReadLine();
+                try
+                {
+                    if (choice == "1") ListOrders();
+                    else if (choice == "2") CreateOrder();
+                    else if (choice == "3") break;
+                    else Console.WriteLine("Ogiltigt val.\n");
+                }
+                catch (DbUpdateException ex)
+                {
+                    Console.WriteLine($"[FEL] Databasfel: {ex.Message}\n");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"[FEL] Okänt fel: {ex.Message}\n");
+                }
+                
+                static void SeedAdmin(ApplicationDbContext db)
+                {
+                    if (!db.Users.Any())
+                    {
+                        var hash = PasswordHash.Hash("admin$23");
+                        db.Users.Add(new User { Username = "admin", PasswordHash = hash } );
+                        db.SaveChanges();
+                        Console.WriteLine(" Skapade admin/admin (lösenord: Admin$23");
+
+                    }
+                }
+                static void ListOrders(ApplicationDbContext db)
+                {
+
+                }
+
+                static void CreateOrder(ApplicationDbContext db)
+                {
+                }
+            }
         }
     }
 }
